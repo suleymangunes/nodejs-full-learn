27759ba9de626057d0ffa08cedf755515095e2ca
suleyman <62201710+suleymangunes@users.noreply.github.com>
2023-02-02 12:58:04 +0300

    some syntax fixed

--- nodejsNedir/02-test.js
+++ nodejsNedir/02-test.js
@@ -2,4 +2,4 @@
 // shell ortamidir
 // .load ile dosya ismi yazilinca satir satir okur ve komut ise komutlari calistirir
 // terminalde node yazilinca acilir
-console.log("bu bir test dosyasidir 03-test.js")
+console.log("Bu bir test dosyasidir 02-test.js")

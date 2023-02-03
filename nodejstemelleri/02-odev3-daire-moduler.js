8ea380a95d22e7d5726b7125e7fbf7ef7806691e
suleyman <62201710+suleymangunes@users.noreply.github.com>
2023-02-03 15:58:57 +0300

    deleted files

--- nodejstemelleri/02-odev3-daire-moduler.js
+++
@@ -1,6 +0,0 @@
-// baska bir js dosyasindan modul seklinde fonksiyonlar alindi ve degiskene atandi
-let circle = require('./02-daire-alan-cevre')
-
-// degisken uzerinden fonksiyonlar cagrildi
-circle.circleArea(5)
-circle.circleCircumference(5)

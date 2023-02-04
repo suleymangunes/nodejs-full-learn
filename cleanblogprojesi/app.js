36c84c7553eeb0aef3be0b4c09d5c2408afa9a81
suleyman <62201710+suleymangunes@users.noreply.github.com>
2023-02-04 22:06:20 +0300

    Middleware static files, Template Engine

--- cleanblogprojesi/app.js
+++ cleanblogprojesi/app.js
@@ -1,18 +1,36 @@
 // express modulu ice  aktarildi
 const express = require('express');
 
+// ejs
+const ejs = require('ejs');
+
 // express modulu degiskene atandi
 const app = express();
 
+// ejs template engine olarak ayarlandi
+app.set('view engine', 'ejs');
+
+// middleware ile staticler public klasorunde bildirimi
+app.use(express.static('public'));
+
 // get ile istekte bulunuldu
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
+  res.render('index');
+  // const blog = {
+  //   id: 1,
+  //   title: 'Blog title',
+  //   description: 'Blog description',
+  // };
 
-  res.send(blog);
+  // res.send(blog);
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 const port = 3000;

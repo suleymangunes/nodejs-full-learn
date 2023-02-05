f3cf01caffc7ec4e8ca9500342d893ce64772648
suleyman <62201710+suleymangunes@users.noreply.github.com>
2023-02-05 22:16:30 +0300

    Connect Database and Added post

--- cleanblogprojesi/app.js
+++ cleanblogprojesi/app.js
@@ -7,15 +7,30 @@ const ejs = require('ejs');
 // express modulu degiskene atandi
 const app = express();
 
+// mongoose paketi ice aktarildi
+const mongoose = require('mongoose');
+const Post = require('./models/Post');
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
+
 // ejs template engine olarak ayarlandi
 app.set('view engine', 'ejs');
 
 // middleware ile staticler public klasorunde bildirimi
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extends: true }));
+app.use(express.json());
+
 // get ile istekte bulunuldu
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({}).sort({ dateCreated: -1 });
+  res.render('index', {
+    posts,
+  });
+  console.log(posts.length);
   // const blog = {
   //   id: 1,
   //   title: 'Blog title',
@@ -31,6 +46,14 @@ app.get('/about', (req, res) => {
 
 app.get('/add_post', (req, res) => {
   res.render('add_post');
+});
+
+app.post('/add', async (req, res) => {
+  console.log('gidildi');
+  // console.log(req);
+  console.log(req.body);
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
